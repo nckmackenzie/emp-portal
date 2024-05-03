3f170e16ed06c9dcb9abef6beeacdd3f8fa8629b
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-05-03 09:42:10 +0300

    refactor: remove authentication

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -7,22 +7,23 @@ const f = createUploadthing();
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageOrDocumentUploader: f(['image', 'pdf'])
-    .middleware(async ({ req }) => {
-      const { user } = await validateRequest();
+    // .middleware(async ({ req }) => {
+    //   // const { user } = await validateRequest();
+    //   // console.log(user);
 
-      if (!user) throw new UploadThingError('Unauthorized');
+    //   // if (!user) throw new UploadThingError('Unauthorized');
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
-    })
+    //   // Whatever is returned here is accessible in onUploadComplete as `metadata`
+    //   return { userId: user.id };
+    // })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log('Upload complete for userId:', metadata.userId);
+      // console.log('Upload complete for userId:', metadata.userId);
 
       console.log('file url', file.url);
 
       // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
+      return 'Upload success!';
     }),
 } satisfies FileRouter;
 
