ddca4fddee217b12001596a9d7ac078363ce5d55
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-05-04 11:13:05 +0300

    feat: add function to get leave days

--- src/app/(portal)/leave-application/_services/index.ts
+++ src/app/(portal)/leave-application/_services/index.ts
@@ -1,6 +1,7 @@
 import { cache } from 'react';
 
 import db from '@/lib/database/db';
+import { validateRequest } from '../../../../../auth';
 
 export const getLeaveTypes = cache(async () => {
   return await db.query.leaveTypes.findMany({
@@ -14,3 +15,24 @@ export const getLeaveNo = async () => {
   });
   return result.length + 1;
 };
+
+export const getAppliedLeaves = cache(async () => {
+  const { user } = await validateRequest();
+
+  if (!user)
+    throw new Error('You need to be logged in to perform this action.');
+
+  return await db.query.leaveApplications.findMany({
+    columns: {
+      leaveNo: true,
+      applicationDate: true,
+      daysTaken: true,
+      endDate: true,
+      startDate: true,
+      leaveStatus: true,
+    },
+    where: (model, { eq, and }) =>
+      and(eq(model.isDeleted, false), eq(model.employeeId, user.employeeRefId)),
+    orderBy: (model, { desc }) => [desc(model.applicationDate)],
+  });
+});
