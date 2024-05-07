86a9a63b371546bf1545bb0e17c6176480047d5d
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-05-07 15:14:06 +0300

    feat: employee details form

---
+++ src/app/(portal)/my-details/page.tsx
@@ -0,0 +1,25 @@
+import { Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+import {
+  getEmployeeChildren,
+  getEmployeeDetails,
+  getEmployeeNok,
+} from './_services';
+import DetailsForms from './_components/details-form';
+import { validateRequest } from '../../../../auth';
+
+export const metadata: Metadata = {
+  title: 'My details',
+};
+
+export default async function MyDetailsPage() {
+  const { user } = await validateRequest();
+  if (!user) redirect('/login');
+
+  const info = await getEmployeeDetails(user.employeeRefId);
+  const nextOfKin = await getEmployeeNok(user.employeeRefId);
+  const children = await getEmployeeChildren(user.employeeRefId);
+
+  return <DetailsForms data={info} nok={nextOfKin} kids={children} />;
+}
