1b22ce550df509ccba43e900157b6961fbe3d6c2
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-05-08 12:49:56 +0300

    chore: add util function to get initials from name

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,18 @@ export const titleCase = (str: string) => {
 export const dummyArray = (length: number) => {
   return Array.from({ length: length || 5 });
 };
+
+export function getInitials(fullName: string | null): string {
+  if (!fullName) return 'U';
+
+  const names = fullName.trim().split(/\s+/);
+  if (names.length === 1) {
+    return names[0][0].toUpperCase();
+  } else if (names.length === 2) {
+    return names.map(name => name[0].toUpperCase()).join('');
+  } else {
+    return `${names[0][0].toUpperCase()}${names[
+      names.length - 1
+    ][0].toUpperCase()}`;
+  }
+}
