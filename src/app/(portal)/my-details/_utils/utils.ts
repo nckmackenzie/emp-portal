d5e4023eab8f52e0c85aa8e4b595217f996dc1e6
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-05-08 16:10:44 +0300

    chore: add array for blood type and tabs

---
+++ src/app/(portal)/my-details/_utils/utils.ts
@@ -0,0 +1,13 @@
+export const EMPLOYEE_TABS = [
+  'Personal Information',
+  'Statutory Details',
+  'NOK/Dependants',
+  'Other details',
+];
+
+export const BLOOD_TYPES = [
+  { value: 'A', label: 'A' },
+  { value: 'B', label: 'B' },
+  { value: 'AB', label: 'AB' },
+  { value: 'O', label: 'O' },
+];
