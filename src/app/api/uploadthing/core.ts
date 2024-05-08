2f255dc4d88735c07576126c2d37c28b8e8730c7
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-05-08 12:50:24 +0300

    chore: add endpoint for image only

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -5,24 +5,26 @@ import { validateRequest } from '../../../../auth';
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageOrDocumentUploader: f(['image', 'pdf'])
-    // .middleware(async ({ req }) => {
-    //   // const { user } = await validateRequest();
-    //   // console.log(user);
+  imageUploader: f({ image: { maxFileSize: '4MB' } })
+    .middleware(async ({ req }) => {
+      const { user } = await validateRequest();
+      if (!user) throw new Error('Unauthorized');
 
-    //   // if (!user) throw new UploadThingError('Unauthorized');
-
-    //   // Whatever is returned here is accessible in onUploadComplete as `metadata`
-    //   return { userId: user.id };
-    // })
+      return { userId: user.id };
+    })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
-      // console.log('Upload complete for userId:', metadata.userId);
+      return { uploadedBy: metadata.userId };
+    }),
+  imageOrDocumentUploader: f(['image', 'pdf'])
+    .middleware(async ({ req }) => {
+      const { user } = await validateRequest();
+      console.log(user);
 
-      console.log('file url', file.url);
+      if (!user) throw new UploadThingError('Unauthorized');
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
       return 'Upload success!';
     }),
 } satisfies FileRouter;
