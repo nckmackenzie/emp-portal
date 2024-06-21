977c2572a825f4c859271ac7a2d5232c2895c1f6
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-21 14:41:03 +0300

    style: sidebar logo

---
+++ src/components/layout/logo.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
+
+import { useStore } from '@/hooks/use-store';
+import { useSidebarToggle } from '@/hooks/use-sidebar-toggle';
+
+export default function Logo() {
+  const { theme } = useTheme();
+  const sidebar = useStore(useSidebarToggle, state => state);
+
+  if (!sidebar) return null;
+
+  return sidebar.isOpen ? (
+    <Image
+      src={theme === 'light' ? '/logos/logo-black.png' : '/logos/logo-dark.svg'}
+      alt="panesar logo"
+      className="h-10 w-auto"
+      width={1200}
+      height={960}
+    />
+  ) : (
+    <Image
+      src={
+        theme === 'light'
+          ? '/logos/favicon-black.svg'
+          : '/logos/favicon-white.svg'
+      }
+      alt="panesar logo"
+      width={36}
+      height={36}
+      className="size-8"
+    />
+  );
+}
