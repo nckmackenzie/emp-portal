e8fe294a398c27b38d1da26cf6340e7d271e5c4a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-28 16:05:20 +0300

    feat: pending loans table

---
+++ src/app/(portal)/staff-loans/_components/pending-loans.tsx
@@ -0,0 +1,64 @@
+import { format } from 'date-fns';
+
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
+import { checkUserAuth } from '@/lib/auth';
+import db from '@/lib/database/db';
+import { numberFormat } from '@/lib/formatters';
+
+export default async function PendingLoansTable() {
+  const user = await checkUserAuth();
+  const pending = await db.query.staffLoans.findMany({
+    columns: {
+      id: true,
+      loanDate: true,
+      loanAmount: true,
+      loanDuration: true,
+      deductableAmount: true,
+    },
+    where: (model, { eq, and }) =>
+      and(
+        eq(model.loanStatus, 'PENDING'),
+        eq(model.employeeId, user.employeeRefId)
+      ),
+  });
+
+  if (!pending.length) return null;
+
+  return (
+    <Table>
+      <TableCaption>Loan(s) pending approval.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Application Date</TableHead>
+          <TableHead className="text-right">Loan Amount</TableHead>
+          <TableHead className="text-right">Loan Duration</TableHead>
+          <TableHead className="text-right">Monthly Payment</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {pending.map(loan => (
+          <TableRow key={loan.id}>
+            <TableCell>{format(new Date(loan.loanDate), 'PPP')}</TableCell>
+            <TableCell className="font-medium text-right">
+              {numberFormat(loan.loanAmount)}
+            </TableCell>
+            <TableCell className="text-right">
+              {loan.loanDuration} Months
+            </TableCell>
+            <TableCell className="text-right">
+              {numberFormat(loan.deductableAmount)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
