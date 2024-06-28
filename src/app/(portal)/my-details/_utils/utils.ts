f8a7bb0216d3393d2e4ab420331bcec097c138a5
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-28 09:18:50 +0300

    chore: add array for education details

--- src/app/(portal)/my-details/_utils/utils.ts
+++ src/app/(portal)/my-details/_utils/utils.ts
@@ -12,3 +12,15 @@ export const BLOOD_TYPES = [
   { value: 'AB', label: 'AB' },
   { value: 'O', label: 'O' },
 ];
+
+export const EDUCATION_LEVELS = [
+  { value: 'PHD', label: 'Doctorate(PHD)' },
+  { value: 'MASTER', label: 'Masters Degree' },
+  { value: 'BACHELORS-DEGREE', label: 'Bachelors Degree' },
+  { value: 'HIGHER-DIPLOMA', label: 'Higher Diploma' },
+  { value: 'DIPLOMA', label: 'Diploma' },
+  { value: 'CERTIFICATE', label: 'Certificate' },
+  { value: 'SECONDARY', label: 'Secondary' },
+  { value: 'PRIMARY', label: 'Primary' },
+  { value: 'NONE', label: 'None' },
+];
