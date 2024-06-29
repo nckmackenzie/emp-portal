76aca4705edfedaa3bf374b787f2f5d7a0702e52
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-29 10:02:53 +0300

    style: add border to table

--- src/app/(portal)/staff-loans/_components/pending-loans.tsx
+++ src/app/(portal)/staff-loans/_components/pending-loans.tsx
@@ -33,32 +33,33 @@ export default async function PendingLoansTable() {
   if (!pending.length) return null;
 
   return (
-    <Table>
-      <TableCaption>Loan(s) pending approval.</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Application Date</TableHead>
-          <TableHead className="text-right">Loan Amount</TableHead>
-          <TableHead className="text-right">Loan Duration</TableHead>
-          <TableHead className="text-right">Monthly Payment</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {pending.map(loan => (
-          <TableRow key={loan.id}>
-            <TableCell>{format(new Date(loan.loanDate), 'PPP')}</TableCell>
-            <TableCell className="font-medium text-right">
-              {numberFormat(loan.loanAmount)}
-            </TableCell>
-            <TableCell className="text-right">
-              {loan.loanDuration} Months
-            </TableCell>
-            <TableCell className="text-right">
-              {numberFormat(loan.deductableAmount)}
-            </TableCell>
+    <div className="rounded-md border">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Application Date</TableHead>
+            <TableHead className="text-right">Loan Amount</TableHead>
+            <TableHead className="text-right">Loan Duration</TableHead>
+            <TableHead className="text-right">Monthly Payment</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {pending.map(loan => (
+            <TableRow key={loan.id}>
+              <TableCell>{format(new Date(loan.loanDate), 'PPP')}</TableCell>
+              <TableCell className="font-medium text-right">
+                {numberFormat(loan.loanAmount)}
+              </TableCell>
+              <TableCell className="text-right">
+                {loan.loanDuration} Months
+              </TableCell>
+              <TableCell className="text-right">
+                {numberFormat(loan.deductableAmount)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 }
