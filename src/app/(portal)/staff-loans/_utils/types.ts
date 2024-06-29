933686625fd5f072743a15771629ead97cb403df
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-06-29 09:50:29 +0300

    chore: add type for loan

--- src/app/(portal)/staff-loans/_utils/types.ts
+++ src/app/(portal)/staff-loans/_utils/types.ts
@@ -1,4 +1,13 @@
 import { z } from 'zod';
 import { loanSchema } from './schema';
+import { getLoans } from '../../leaves/leave-application/_services';
 
 export type TLoanApplication = z.infer<typeof loanSchema>;
+
+export type Loan = Awaited<
+  ReturnType<typeof getLoans>
+> extends (infer U)[]
+  ? U
+  : never;
+
+
