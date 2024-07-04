10f159a43eb2255cbe7697af460fa54b6147cf50
nckmackenzie <nicholas.mackenzie1@gmail.com>
2024-07-04 08:18:15 +0300

    feat: fetch and set actual values

--- src/app/(portal)/dashboard/_components/dashboard-cards.tsx
+++ src/app/(portal)/dashboard/_components/dashboard-cards.tsx
@@ -1,20 +1,33 @@
 import { Calculator, Calendar, Clock, Hourglass } from 'lucide-react';
 import DashboardCard from './dashboard.card';
+import { getCummulatedDays, getDaysTaken } from '../_services';
 
-export default function DashboardCards() {
+export default async function DashboardCards() {
+  const [accumulatedDays, daysTaken] = await Promise.all([
+    getCummulatedDays(),
+    getDaysTaken(),
+  ]);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-      <DashboardCard label="Total days accured" value={2} Icon={Calendar} />
+      <DashboardCard
+        label="Total days accured"
+        value={accumulatedDays}
+        Icon={Calendar}
+      />
       <DashboardCard
         label="Total days taken"
-        value={2}
+        value={daysTaken}
         Icon={Calculator}
         href="/leave/applied"
       />
-      <DashboardCard label="Leave balance" value={2} Icon={Hourglass} />
+      <DashboardCard
+        label="Leave balance"
+        value={accumulatedDays - daysTaken}
+        Icon={Hourglass}
+      />
       <DashboardCard
         label="Total lost hours"
-        value={2}
+        value={0}
         Icon={Clock}
         href="/lost-hours"
       />
